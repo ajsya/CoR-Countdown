3ecde15404a3f1771fe7e57fdb337fc57a7cfa1b
Glitch (cor-countdown) <none>
2021-02-02 04:47:08 +0000

    Updated to support synchronous countdown across timezones.

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
+function changeTimezone(date, ianatz) {
+
+  // suppose the date is 12:00 UTC
+  var invdate = new Date(date.toLocaleString('en-US', {
+    timeZone: ianatz
+  }));
+
+  // then invdate will be 07:00 in Toronto
+  // and the diff is 5 hours
+  var diff = date.getTime() - invdate.getTime();
+
+  // so 12:00 in Toronto is 17:00 UTC
+  return new Date(date.getTime() - diff); // needs to substract
+  
+}
+
 // Set the date we're counting down to
-var countDownDate = new Date("Feb 21, 2021 19:34:35").getTime();
+var here = new Date("Feb 21, 2021 19:34:35");
+var there = changeTimezone(here, "Europe/Paris");
+var countDownDate = new Date(there).getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
