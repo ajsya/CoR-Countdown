6fda4ba8ab55a849a219ae871dfa370f46ab747a
ajsya <52182942+ajsya@users.noreply.github.com>
2021-02-12 17:33:44 -0500

    Updated for more accurate date and time.
    23/feb/2021 at 01:34:35 UTC

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ function sendMessage() {
   alert("Message Sent!")
 }
 
-var countDownDate = new Date("Feb 21, 2021 19:34:35 EST").getTime();
+var countDownDate = new Date("Feb 23, 2021 1:34:35 UTC").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
